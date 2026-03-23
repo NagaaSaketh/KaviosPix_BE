c27fd1ea5c636acd218c3e4a604206c143ba7980
Saketh <vnaagsaketh8925@gmail.com>
2026-03-23 10:58:37 +0530

    bug:modified upload middleware

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,7 +1,5 @@
 const multer = require("multer");
 const cloudinary = require("../config/cloudinary");
-const multer = require("multer");
-
 const upload = multer({ dest: "temp/" });
 
 module.exports = upload;
